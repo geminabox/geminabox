60fb12d925d1d6e1e322b26869f5bb983b7ffd77
sue445 <sue445@sue445.net>
2014-09-07 00:22:18 +0900

    Add badge info

--- public/master.js
+++ public/master.js
@@ -2,4 +2,8 @@
   $("form.delete-form").submit(function() {
     return confirm("Are you sure you want to delete this gem?");
   });
+
+  $(".click-select").click(function(){
+    $(this).select();
+  });
 })(jQuery);
