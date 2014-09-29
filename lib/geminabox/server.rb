c3ae4d5f6df6a70bb18516eb0b333019219ad338
sue445 <sue445@sue445.net>
2014-09-30 00:09:16 +0900

    Fix performance issue: load only specify gem when show badge

--- lib/geminabox/server.rb
+++ lib/geminabox/server.rb
@@ -105,7 +105,7 @@ module Geminabox
     end
 
     get '/gems/:gemname.svg' do
-      gems = Hash[load_gems.by_name]
+      gems = Hash[load_param_gem.by_name]
       @gem = gems[params[:gemname]]
       halt 404 unless @gem
       erb :"gem_badge.svg", :layout => false, :content_type => "image/svg+xml"
@@ -217,6 +217,11 @@ HTML
 
     def load_gems
       @loaded_gems ||= Geminabox::GemVersionCollection.new(all_gems)
+    end
+
+    def load_param_gem
+      gem_versions = Geminabox::GemFinder.find_by_name(params[:gemname])
+      Geminabox::GemVersionCollection.new(gem_versions)
     end
 
     def index_gems(gems)
