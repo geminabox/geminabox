8f7c4d66861094413c1c0880b69a3787c55c1873
Ben Biddington <ben.biddington@gmail.com>
2014-11-08 11:01:28 +1300

    Refactor: assume server is running at <http://localhost:9292/>

--- test/system/atom_feed_test.rb
+++ test/system/atom_feed_test.rb
@@ -4,6 +4,10 @@ require 'minitest/unit'
 module GeminaboxSystemTest
   attr_accessor :last_response 
 
+  def self.included(klass)
+    Assume.local_server_running
+  end
+  
   def get(relative_uri)
     @last_response = internet.get "http://localhost:9292#{relative_uri}"
   end
@@ -15,9 +19,24 @@ module GeminaboxSystemTest
   end
 end
 
+module Assume
+  class << self
+    def local_server_running
+      earl = "http://localhost:9292/"
+
+      begin 
+        reply = Geminabox::HttpClientAdapter.new.get earl
+        fail "Your local server is running at <#{earl}>, but returned unexpected status. #{reply.inspect}" unless reply.ok?
+      rescue Exception => e
+        fail "Your local server is not running at <#{earl}>."
+      end
+    end
+  end
+end
+
 class AtomFeedTest < Minitest::Test
   include GeminaboxSystemTest
-
+  
   test "atom feed returns when no gems are defined" do
     get "/atom.xml"
     
