7c16df3c4c5daac18b57c02784be7f11d67da03d
Kerri Miller <kerri.miller@livingsocial.com>
2015-06-01 15:22:29 -0700

    Move content-type into header as required by latest version of webmock

--- test/units/geminabox/rubygems_dependency_test.rb
+++ test/units/geminabox/rubygems_dependency_test.rb
@@ -11,7 +11,7 @@ module Geminabox
 
     def test_get_list
       stub_request(:get, "https://bundler.rubygems.org/api/v1/dependencies.json?gems=some_gem,other_gem").
-        to_return(:status => 200, :body => some_gem_dependencies.to_json, :headers => {}, :content_type => 'application/json')
+        to_return(:status => 200, :body => some_gem_dependencies.to_json, :headers => {"Content-Type" => 'application/json'})
 
       assert_equal some_gem_dependencies, RubygemsDependency.for(:some_gem, :other_gem)
     end
