c0fc55bf96cf4aee0f21536881a5d1e3893cea0a
Stewart Matheson <stew@rtmatheson.com>
2015-06-27 14:18:43 +1000

    Updated external dependencys to fail over correctly if one dependency is down.

--- lib/geminabox/external_dependency.rb
+++ lib/geminabox/external_dependency.rb
@@ -6,16 +6,19 @@ module Geminabox
     class << self
 
       def for(*gems)
-        source_list = []
-        Sources.external_sources.each do |source|
-          url = [ external_uri(source), '?gems=', gems.map(&:to_s).join(',') ].join
-          body = Geminabox.http_adapter.get_content(url)
-          source_list << JSON.parse(body)
+        source_list = Sources.external_sources.inject([]) do |memo, source|
+          memo << for_source(gems, source)
         end
         source_list.flatten.uniq
-      rescue Exception => e
+      rescue SocketError, HTTPClient::BadResponseError => e
         return [] if Geminabox.allow_remote_failure
         raise e
+      end
+
+      def for_source gems, source
+        url = [ external_uri(source), '?gems=', gems.map(&:to_s).join(',') ].join
+        body = Geminabox.http_adapter.get_content(url)
+        JSON.parse(body)
       end
 
       def external_uri(source)
