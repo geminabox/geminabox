f76aac7f965315f53dc87773325aa7cb5298c66c
Aaron Averbuch <aaron.averbuch@placed.com>
2016-02-09 08:47:48 -0500

    Update default rubygems url and add config.ru override

--- lib/geminabox/version.rb
+++ lib/geminabox/version.rb
@@ -1,3 +1,7 @@
 module Geminabox
+<<<<<<< HEAD
   VERSION = '0.13.5' unless defined? VERSION
+=======
+  VERSION = '0.12.4.1' unless defined? VERSION
+>>>>>>> 8d6660b... Update default rubygems url and add config.ru override
 end
