39f5b2ff29be21895285c00654c73981b3750271
sonots <sonots@gmail.com>
2016-04-02 17:37:49 +0900

    speedup gem_list_merge
    BEFORE: 1.10931879 sec
    AFTER: 0.047122157
    
    to resolve dependencies for
    bones,rcov,rspec,rubyforge,ZenTest,addressable,multipart-post,faraday,faraday-middleware,httpauth,jwt,multi_xml,jar-dependencies,ffi,rbnacl,curses,launchy,bones-rcov,bones-rspec,bones-rubyforge,bones-zentest,git,functional-ruby,ref,gherkin,gherkin3,trollop,term-ansicolor,hashie,oauth2,nokogiri,termios,json_pure,RubyInline,metaclass,needle,echoe,jruby-pageant,bcrypt_pbkdf,rbnacl-libsodium,minitest,psych,tins,facets,polyglot,unf_ext,sexp_processor,concurrent-ruby,method_source,jamespath
    
    ```ruby
    require 'geminabox'
    
    local_gem_list = Marshal.load(File.read('bench/local_gem_list_bones.marshal'))
    remote_gem_list = Marshal.load(File.read('bench/remote_gem_list_bones.marshal'))
    
    started = Time.now
    combined_gem_list = Geminabox::GemListMerge.from(local_gem_list, remote_gem_list)
    elaspsed = Time.now - started
    puts "#{elaspsed.to_f} sec"
    
    expected = Marshal.load(File.read('bench/combined_gem_list_bones.marshal'))
    puts combined_gem_list == expected
    ```

--- lib/geminabox/gem_list_merge.rb
+++ lib/geminabox/gem_list_merge.rb
@@ -2,6 +2,8 @@ module Geminabox
   class GemListMerge
     attr_accessor :list
 
+    IGNORE_DEPENDENCIES = 0..-2
+
     def self.from(*lists)
       lists.map{|list| new(list)}.inject(:merge)
     end
@@ -11,40 +13,10 @@ module Geminabox
     end
 
     def merge(other)
-      combine_hashes(other).values.flatten.sort do |x, y|
-        x.values[ignore_dependencies] <=> y.values[ignore_dependencies]
-      end
-    end
-
-    def hash
-      list.each do |item|
-        ensure_symbols_as_keys(item)
-        name = item[:name].to_sym
-        collection[name] ||= []
-        collection[name] << item unless collection[name].include?(item)
-      end
-      collection
-    end
-
-    def collection
-      @collection ||= {}
-    end
-
-    def combine_hashes(other)
-      hash.merge(other.hash) do |key, value, other_value|
-        (value + other_value).uniq{|v| v.values[ignore_dependencies]}
-      end
-    end
-
-    def ignore_dependencies
-      0..-2
-    end
-
-    def ensure_symbols_as_keys(item)
-      item.keys.each do |key|
-        next if key.kind_of? Symbol
-        item[key.to_sym] = item.delete(key)
-      end
+      merged = (list + other.list)
+      merged.uniq! {|val| val.values[IGNORE_DEPENDENCIES] }
+      merged.sort_by! {|x| x.values[IGNORE_DEPENDENCIES] }
+      merged
     end
 
   end
