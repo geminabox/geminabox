bab1d711298f74c315747affa3063f3ef72ff030
Takuya Noguchi <takninnovationresearch@gmail.com>
2022-06-25 08:19:09 +0000

    Prettier JavaScript files
    Fix lints in PR 361: https://github.com/geminabox/geminabox/pull/361
    
    Signed-off-by: Takuya Noguchi <takninnovationresearch@gmail.com>

--- public/master.js
+++ public/master.js
@@ -1,7 +1,11 @@
-[].forEach.call(document.querySelectorAll("form.delete-form"), function(el) {
-  el.addEventListener("submit", function(ev) {
-    if (!confirm("Are you sure you want to delete this gem?")) {
-      ev.preventDefault();
-    }
-  }, false);
+[].forEach.call(document.querySelectorAll("form.delete-form"), function (el) {
+  el.addEventListener(
+    "submit",
+    function (ev) {
+      if (!confirm("Are you sure you want to delete this gem?")) {
+        ev.preventDefault();
+      }
+    },
+    false
+  );
 });
