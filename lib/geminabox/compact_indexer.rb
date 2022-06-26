d499d5fd9023e3832b99a2bb59f3b1620a97357f
Stefan Kaes <stefan.kaes@xing.com>
2022-06-26 15:11:23 +0200

    extract compact indexer into separate module

--- lib/geminabox/compact_indexer.rb
+++ lib/geminabox/compact_indexer.rb
@@ -43,6 +43,84 @@ module Geminabox
         File.join(info_path, name)
       end
 
+      def log_time(text)
+        start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+        yield
+      ensure
+        end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+        printf("%s: %.2f seconds\n", text, end_time - start_time)
+      end
+
+      def incremental_reindex_compact_cache(gem_specs)
+        version_info = VersionInfo.new
+        version_info.load_versions
+
+        gem_specs.group_by(&:name).each do |name, specs|
+          info = DependencyInfo.new(name)
+          data = CompactIndexer.fetch_info(name)
+          info.content = data if data
+          specs.each do |spec|
+            gem_version = GemVersion.new(name, spec.version, spec.platform)
+            checksum = Specs.checksum_for_version(gem_version)
+            info.add_gem_spec_and_gem_checksum(spec, checksum)
+          end
+          CompactIndexer.reindex_info(name, info.content)
+          version_info.update_gem_versions(info)
+        end
+
+        version_info.write
+      end
+
+      def all_specs
+        Geminabox::GemVersionCollection.new(Specs.all_gems)
+      end
+
+      def full_reindex_compact_cache
+        CompactIndexer.clear_index
+        version_info = VersionInfo.new
+
+        all_specs.by_name.to_h.each do |name, versions|
+          info = dependency_info(versions)
+          CompactIndexer.reindex_info(name, info.content)
+          version_info.update_gem_versions(info)
+        end
+
+        version_info.write
+      end
+
+      def reindex_compact_cache(specs = nil)
+        return unless Geminabox.index_format
+
+        if specs && File.exist?(CompactIndexer.versions_path)
+          log_time("compact index incremental reindex") do
+            incremental_reindex_compact_cache(specs)
+          end
+          return
+        end
+
+        log_time("compact index full rebuild") do
+          full_reindex_compact_cache
+        end
+      rescue SystemCallError => e
+        CompactIndexer.clear_index
+        puts "Compact index error #{e.message}\n"
+      end
+
+      def dependency_info(gem)
+        DependencyInfo.new(gem.first.name) do |info|
+          gem.by_name do |_name, versions|
+            versions.each do |version|
+              spec = Specs.spec_for_version(version)
+              next unless spec
+
+              checksum = Specs.checksum_for_version(version)
+              next unless checksum
+
+              info.add_gem_spec_and_gem_checksum(spec, checksum)
+            end
+          end
+        end
+      end
     end
   end
 end
--- lib/geminabox/indexer.rb
+++ lib/geminabox/indexer.rb
@@ -38,7 +38,7 @@ module Geminabox
       if force_rebuild
         indexer.generate_index
         Server.dependency_cache.flush
-        reindex_compact_cache
+        CompactIndexer.reindex_compact_cache
       else
         begin
           updated_gemspecs = self.class.updated_gemspecs(indexer)
@@ -48,7 +48,7 @@ module Geminabox
           updated_gemspecs.each do |spec|
             Server.dependency_cache.flush_key(spec.name)
           end
-          reindex_compact_cache(updated_gemspecs)
+          CompactIndexer.reindex_compact_cache(updated_gemspecs)
         rescue Errno::ENOENT
           Server.with_rlock { reindex(:force_rebuild) }
         rescue StandardError => e
@@ -58,85 +58,5 @@ module Geminabox
         end
       end
     end
-
-    def log_time(text)
-      start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-      yield
-    ensure
-      end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-      printf("%s: %.2f seconds\n", text, end_time - start_time)
-    end
-
-    def incremental_reindex_compact_cache(gem_specs)
-      version_info = VersionInfo.new
-      version_info.load_versions
-
-      gem_specs.group_by(&:name).each do |name, specs|
-        info = DependencyInfo.new(name)
-        data = CompactIndexer.fetch_info(name)
-        info.content = data if data
-        specs.each do |spec|
-          gem_version = GemVersion.new(name, spec.version, spec.platform)
-          checksum = Specs.checksum_for_version(gem_version)
-          info.add_gem_spec_and_gem_checksum(spec, checksum)
-        end
-        CompactIndexer.reindex_info(name, info.content)
-        version_info.update_gem_versions(info)
-      end
-
-      version_info.write
-    end
-
-    def all_specs
-      Geminabox::GemVersionCollection.new(Specs.all_gems)
-    end
-
-    def full_reindex_compact_cache
-      CompactIndexer.clear_index
-      version_info = VersionInfo.new
-
-      all_specs.by_name.to_h.each do |name, versions|
-        info = info_template(versions)
-        CompactIndexer.reindex_info(name, info.content)
-        version_info.update_gem_versions(info)
-      end
-
-      version_info.write
-    end
-
-    def reindex_compact_cache(specs = nil)
-      return unless Geminabox.index_format
-
-      if specs && File.exist?(CompactIndexer.versions_path)
-        log_time("compact index incremental reindex") do
-          incremental_reindex_compact_cache(specs)
-        end
-        return
-      end
-
-      log_time("compact index full rebuild") do
-        full_reindex_compact_cache
-      end
-    rescue SystemCallError => e
-      CompactIndexer.clear_index
-      puts "Compact index error #{e.message}\n"
-    end
-
-    def info_template(gem)
-      DependencyInfo.new(gem.first.name) do |info|
-        gem.by_name do |_name, versions|
-          versions.each do |version|
-            spec = Specs.spec_for_version(version)
-            next unless spec
-
-            checksum = Specs.checksum_for_version(version)
-            next unless checksum
-
-            info.add_gem_spec_and_gem_checksum(spec, checksum)
-          end
-        end
-      end
-    end
-
   end
 end
